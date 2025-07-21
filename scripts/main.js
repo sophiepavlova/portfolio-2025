f18eaecb104403ce38a10ad28dd039473fa091bb
sophiepavlova <olni.pavlova@gmail.com>
2025-07-21 12:28:43 -0700

    Temporal save

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 console.log("Hello from JS 👾");
+
+const currentPage = location.pathname;
+document.querySelectorAll('.site-nav a').forEach(link => {
+  if (link.getAttribute('href') === currentPage) {
+    link.classList.add('active');
+  }
+});
